3bd6a2f8627ec56c630d595d5c684e544ccb7a4e
sojians <81723405+sojians@users.noreply.github.com>
2021-06-19 17:01:50 -0700

    Add files via upload

---
+++ src/dao/DishesDao.java
@@ -0,0 +1,89 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import entity.Dishes;
+
+
+public class DishesDao {
+
+	public static List<Dishes> findDishes() {
+		try(Connection connection = DbConnection.getConnection();) {
+			String sql = "SELECT * FROM dish ORDER BY dish_id";
+			
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				try(ResultSet rs = statement.executeQuery()) {
+					List<Dishes> dishes = new LinkedList<>();
+					
+					while(rs.next()) {
+						int dishId = rs.getInt("dish_id");
+						String dishName = rs.getString("dish_name");
+						String orderDate = rs.getString("order_date");
+						String dishComment = rs.getString("dish_comment");
+						double dishPrice = rs.getDouble("dish_price");
+						int dishScore = rs.getInt("dish_score");
+						
+						Dishes dish = new Dishes(dishId, dishName, orderDate, dishComment, dishPrice, dishScore);
+						dishes.add(dish);
+					}
+					
+					return dishes;
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void updateDish(int dishId, String dishName, String orderDate, String dishComment, double dishPrice,
+			int dishScore) {
+		try(Connection connection = DbConnection.getConnection()) {
+			String sql = "UPDATE dish SET dish_name = ? WHERE dish_id = ?";
+			try(PreparedStatement statement = connection.prepareStatement(sql)){
+
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	
+	
+
+	public static void createDish(String dishName, String orderDate, String dishComment, double dishPrice,
+			int dishScore) {
+		String sql = "INSERT INTO dishes (dishName, orderDate, dishComment, dishPrice, dishScore) VALUES (?, ?, ?, ?, ?)";
+		try(Connection connection = DbConnection.getConnection()) {
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				statement.setString(1, dishName);
+				statement.setString(2, orderDate);
+				statement.setString(3, dishComment);
+				statement.setDouble(4, dishPrice);
+				statement.setInt(5, dishScore);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e ) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+
+	public static void deleteDish(int dishId) {
+		try(Connection connection = DbConnection.getConnection()) {
+			String sql = "DELETE FROM dish WHERE dish_id = ?";
+			try(PreparedStatement statement = connection.prepareStatement(sql)){
+				statement.setInt(1, dishId);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ src/dao/RestaurantsDao.java
@@ -0,0 +1,79 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import entity.Restaurants;
+
+
+public class RestaurantsDao {
+
+	public static List<Restaurants> findResta() {
+	 	try(Connection connection = DbConnection.getConnection();) {
+			String sql = "SELECT * FROM restaurant ORDER BY restaurant_id";
+			
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				try(ResultSet rs = statement.executeQuery()) {
+					List<Restaurants> restaurants = new LinkedList<>();
+					
+					while(rs.next()) {
+						int restaId = rs.getInt("restaurant_id");
+						String restaName = rs.getString("resta_name");
+						String visitDate = rs.getString("visit_date");
+						String restaCity = rs.getString("resta_city");
+						int restaScore = rs.getInt("resta_score");
+						
+						Restaurants restaurant = new Restaurants(restaId, restaName, visitDate, restaCity, restaScore);
+						restaurants.add(restaurant);
+					}
+					return restaurants;
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public static void updateResta(int restaId, String restaName, String restaCity, String visitDate, int restaScore) {
+		try(Connection connection = DbConnection.getConnection()) {
+			String sql = "UPDATE restaurant SET resta_name = ? WHERE resta_id = ?";
+			try(PreparedStatement statement = connection.prepareStatement(sql)){
+
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static void deleteResta(int restaId) {
+		try(Connection connection = DbConnection.getConnection()) {
+			String sql = "DELETE FROM restaurant WHERE resta_id = ?";
+			try(PreparedStatement statement = connection.prepareStatement(sql)){
+				statement.setInt(1, restaId);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void createResta(String restaName, String restaCity, String visitDate, int restaScore) {
+		String sql = "INSERT INTO restaurants (restaName, restaCity, visitDate, restaScore) VALUES (?, ?, ?, ?)";
+		try(Connection connection = DbConnection.getConnection()) {
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				statement.setString(1, restaName);
+				statement.setString(2, restaCity);
+				statement.setString(3, visitDate);
+				statement.setInt(4, restaScore);
+				statement.executeUpdate();
+			}
+		} catch (SQLException e ) {
+			throw new RuntimeException(e);
+		}
+	}
+}
